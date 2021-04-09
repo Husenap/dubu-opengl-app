a56730dbfd25fae206308f6429ff7bfb2777da16
Hussein Taher <6496177+Husenap@users.noreply.github.com>
2021-04-10 01:52:45 +0200

    updated glad flag to glad2

--- thirdparty/imgui.cmake
+++ thirdparty/imgui.cmake
@@ -37,7 +37,7 @@ if(NOT imgui_POPULATED)
         "${imgui_SOURCE_DIR}"
         "${imgui_SOURCE_DIR}/..")
 
-    target_compile_definitions(imgui PRIVATE -DIMGUI_IMPL_OPENGL_LOADER_GLAD)
+    target_compile_definitions(imgui PRIVATE -DIMGUI_IMPL_OPENGL_LOADER_GLAD2)
     target_link_libraries(imgui glad glfw)
 
     set_target_properties(imgui PROPERTIES FOLDER "thirdparty/imgui")
